cbc399ab6057b8d83107a28418acccdf4bae77ae
mitrofanzxc <mitrofanzxc@gmail.com>
2022-09-19 00:43:25 +0300

    feat: add table to main

--- src/pages/layout/components/header/Header.tsx
+++ src/pages/layout/components/header/Header.tsx
@@ -1,32 +1,36 @@
 import { FC } from 'react';
 import { useGetAssetsQuery } from '../../../../services/coincap';
+import './Header.scss';
 
 const Header: FC = () => {
   const { data: assets, isLoading } = useGetAssetsQuery({ limit: 3 });
-  console.log('data', assets ? assets.data : []);
-  console.log('timestamp', assets ? assets.timestamp : 0);
 
   return (
-    <>
-      {isLoading && <div>Loading...</div>}
-      {!isLoading &&
-        assets &&
-        assets.data.map(({ id, name, symbol, priceUsd }) => {
-          return (
-            <div key={id}>
-              <div>{`Name: ${name}`}</div>
-              <div>{`Price: ${new Intl.NumberFormat('en-US', {
-                style: 'currency',
-                currency: 'USD',
-              }).format(+priceUsd)}`}</div>
-              <img
-                src={`https://assets.coincap.io/assets/icons/${symbol.toLowerCase()}@2x.png`}
-                alt={symbol}
-              />
-            </div>
-          );
-        })}
-    </>
+    <header>
+      <div className="header-currencies">
+        {isLoading && <div>Loading...</div>}
+        {!isLoading &&
+          assets &&
+          assets.data.map(({ id, name, symbol, priceUsd }) => {
+            return (
+              <div key={id} className="header-currency__wrapper">
+                <img
+                  src={`https://assets.coincap.io/assets/icons/${symbol.toLowerCase()}@2x.png`}
+                  alt={symbol}
+                />
+                <div className="header-currency__name">
+                  <div>{`${name}`}</div>
+                  <div>{`${symbol}`}</div>
+                </div>
+                <div>{`${new Intl.NumberFormat('en-US', {
+                  style: 'currency',
+                  currency: 'USD',
+                }).format(+priceUsd)}`}</div>
+              </div>
+            );
+          })}
+      </div>
+    </header>
   );
 };
 
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,9 +1,59 @@
 import { FC } from 'react';
+import { useGetAssetsQuery } from '../../services/coincap';
+import './Main.scss';
 
 const Main: FC = () => {
+  const { data: assets, isLoading } = useGetAssetsQuery({ limit: 20 });
   return (
     <>
-      <div>Main</div>
+      <div>
+        <table className="table">
+          <thead>
+            <tr className="tr">
+              <th colSpan={1}>Rank</th>
+              <th colSpan={2}>Name</th>
+              <th colSpan={1}>Price</th>
+              <th colSpan={1}>Market Cap</th>
+              <th colSpan={1}>VWAP (24Hr)</th>
+              <th colSpan={1}>Supply</th>
+              <th colSpan={1}>Volume (24Hr)</th>
+              <th colSpan={1}>Change (24Hr)</th>
+            </tr>
+          </thead>
+          <tbody>
+            {isLoading && <div>Loading...</div>}
+            {!isLoading &&
+              assets &&
+              assets.data.map(
+                ({
+                  id,
+                  rank,
+                  name,
+                  symbol,
+                  priceUsd,
+                  marketCapUsd,
+                  vwap24Hr,
+                  supply,
+                  volumeUsd24Hr,
+                  changePercent24Hr,
+                }) => {
+                  return (
+                    <tr key={id} className="tr">
+                      <th colSpan={1}>{rank}</th>
+                      <th colSpan={2}>{name}</th>
+                      <th colSpan={1}>{priceUsd}</th>
+                      <th colSpan={1}>{marketCapUsd}</th>
+                      <th colSpan={1}>{vwap24Hr}</th>
+                      <th colSpan={1}>{supply}</th>
+                      <th colSpan={1}>{volumeUsd24Hr}</th>
+                      <th colSpan={1}>{changePercent24Hr}</th>
+                    </tr>
+                  );
+                }
+              )}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
