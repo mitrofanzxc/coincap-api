8aaec1b15fc21142ba110211297ba6b341b24098
mitrofanzxc <mitrofanzxc@gmail.com>
2022-09-28 20:01:57 +0300

    feat: add difference state to portfolio

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,9 +1,9 @@
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { useGetAssetsQuery } from '../../services/coincap';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { open } from '../../features/modalPortfolioToggleSlice';
 import { handleTotalPortfolio, updatePortfolio } from '../../features/portfolioSlice';
-import { convertToThousands } from '../../utils';
+import { convertToThousands, getLocalStorage } from '../../utils';
 import portfolio from '../../assets/images/portfolio.svg';
 
 const Portfolio: FC = () => {
@@ -16,8 +16,21 @@ const Portfolio: FC = () => {
   const currentPortfolioList = useAppSelector(({ portfolio }) => portfolio.portfolioList);
   const currentPortfolioTotal = useAppSelector(({ portfolio }) => portfolio.total);
 
+  const [previousTotal, setPreviousTotal] = useState<number>(
+    getLocalStorage('currentPortfolioTotal')
+  );
+  const [difference, setDifference] = useState<number>(0);
+
   const ids = currentPortfolioList.map(({ id }) => id).join(',');
   const { data: assets } = useGetAssetsQuery({ ids });
+
+  const handleDifference = (prev: number, next: number) => {
+    if (prev < next) {
+      setDifference(prev + next);
+    } else if (prev > next) {
+      setDifference(next - prev);
+    }
+  };
 
   useEffect(() => {
     if (assets?.data) {
@@ -30,6 +43,9 @@ const Portfolio: FC = () => {
 
     const totalSum =
       currentPortfolioList.reduce((prev, next) => prev + +next.priceUsd * next.amount, 0) || 0;
+
+    handleDifference(previousTotal, totalSum);
+
     dispatch(handleTotalPortfolio(totalSum));
   }, [currentPortfolioList]);
 
@@ -39,7 +55,11 @@ const Portfolio: FC = () => {
 
   return (
     <div className="portfolio-wrapper" onClick={openModal}>
+      {/* <div>{`Total: ${convertToThousands(
+        currentPortfolioTotal.toString()
+      )} ${previousTotal} (${persentage} %)`}</div> */}
       <div>{`Total: ${convertToThousands(currentPortfolioTotal.toString())}`}</div>
+      <div>{`Difference: ${convertToThousands(difference.toString())}`}</div>
       <img src={portfolio} alt="portfolio" className="logo" />
     </div>
   );
